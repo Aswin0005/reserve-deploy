6120116186f0c072f74802d3ae25f636b41a59cd
Aswin <aswinash19@gmail.com>
2024-07-10 23:24:26 +0530

    Auth Api route created

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,4 +1,5 @@
 'use client';
+
 const axios = require('axios');
 
 const axiosApi = axios.create({
@@ -9,30 +10,17 @@ const onSubmit = async () => {
   try {
     const response = await axiosApi.post('/register', {
       name: 'Developer',
-      email: 'devp9@gmail.com',
+      email: 'dev2@gmail.com',
       password: '12345678',
     });
-  } catch (error) {
-    console.log(error.response.data.error);
-  }
-};
-
-const LogOut = async () => {
-  try {
-    const response = await axiosApi.get('/logout');
     console.log(response);
   } catch (error) {
-    console.log(error.response.data.error);
+    console.log('Error', error);
   }
 };
 
 const Register = () => {
-  return (
-    <>
-      <button className='bg-red-300' onClick={onSubmit}>Submit</button>;
-      <button onClick={LogOut}>LogOut</button>
-    </>
-  );
+  return <main>Register</main>;
 };
 
 export default Register;
