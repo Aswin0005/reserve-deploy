00bf18d9f49397e9f82bc7116f602b25a63f1ad5
Aswin <aswinash19@gmail.com>
2024-07-10 23:02:52 +0530

    API auth Route

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,5 +1,4 @@
-"use client"
-
+'use client';
 const axios = require('axios');
 
 const axiosApi = axios.create({
@@ -10,17 +9,30 @@ const onSubmit = async () => {
   try {
     const response = await axiosApi.post('/register', {
       name: 'Developer',
-      email: 'dev2@gmail.com',
+      email: 'devp9@gmail.com',
       password: '12345678',
     });
-    console.log(response)
   } catch (error) {
-    console.log('Error',error)
+    console.log(error.response.data.error);
+  }
+};
+
+const LogOut = async () => {
+  try {
+    const response = await axiosApi.get('/logout');
+    console.log(response);
+  } catch (error) {
+    console.log(error.response.data.error);
   }
 };
 
 const Register = () => {
-  return <main>Register</main>;
+  return (
+    <>
+      <button className='bg-red-300' onClick={onSubmit}>Submit</button>;
+      <button onClick={LogOut}>LogOut</button>
+    </>
+  );
 };
 
 export default Register;
---
+++ src/app/api/login/route.js
@@ -0,0 +1,45 @@
+import { NextResponse } from 'next/server';
+import BadRequestError from '../../../../errors/badRequestErrror';
+import NotFoundError from '../../../../errors/notFoundError';
+import UnAuthorizedError from '../../../../errors/unauthorizedError';
+import User from '../../../../models/user';
+import Token from '../../../../models/token';
+import { dbConnect } from '../../../../utils/dbConnect';
+const crypto = require('crypto');
+import { sendCookies } from '../../../../utils/cookies';
+
+export async function POST(request) {
+  const { email, password } = await request.json();
+  await dbConnect();
+  if (!email || !password) {
+    throw BadRequestError('Must Provide both email and password');
+  }
+  const user = await User.findOne({ email: email }).select('_id name email')
+
+  if (!user) {
+    throw NotFoundError('User Not Found');
+  }
+
+  const isPasswordMatch = user.comparePassword(password);
+
+  if (!isPasswordMatch) {
+    throw UnAuthorizedError('Credients in Invalid');
+  }
+
+  const cryptoRefToken = crypto.randomBytes(40).toString('hex');
+
+  //Creating Unique refresh token to user
+  const refToken = await Token.create({
+    refreshToken: cryptoRefToken,
+    user: user._id,
+  })
+
+  //Destructure RefreshHash and UserId
+  const refreshTokenUser = {
+    refreshToken: refToken.refreshToken,
+    user: refToken.user,
+  };
+
+  sendCookies(user, refreshTokenUser);
+  return NextResponse.json({ user }, { status: 200 });
+}
