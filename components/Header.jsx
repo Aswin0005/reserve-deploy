8607ed3c1ea3b25d473593e91225542c30ea5ba5
dineshg04 <dineshkumarg2206@gmail.com>
2024-07-30 22:47:12 +0530

    search functionality

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { Search } from 'react-feather';
 import axios from 'axios';
 import Error from 'next/error';
+import { useState, useEffect } from 'react';
 
 const axiosApi = axios.create({
   baseURL: 'http://localhost:3000/api',
@@ -17,6 +18,43 @@ function Header({ totalUniqueItems }) {
       throw new Error('Error Logging Out', error);
     }
   };
+const restaurantdata =[
+  {
+    id: 1,
+    name:"buhari"
+  },
+  {
+    id:2,
+    name:"Adayar Ananda Bhavan"
+  },
+  {
+    id:3 ,
+    name:"AL Bhazeer"
+  },
+  {
+    id:4 ,
+    name:"The Cake Shop"
+  }
+];
+
+const [restaurant,setRestaurant] = useState('');
+const handlesearch = document.getElementById('hotellist');
+function handlesearching(e){
+  if (handlesearch) {
+    if(e.target.value ===""){
+      handlesearch.style.display = 'none';
+    }
+    else{
+      handlesearch.style.display = 'inline-block';
+
+    }
+  };
+};
+
+
+        
+
+   
 
   return (
     <nav className=" whitespace-nowrap flex gap-6 bg-white h-14 items-center px-5 shadow-md sm:flex sm:bg-white sm:py-4 sm:px-4 sm:px-6 lg:px-8 sm:flex sm:items-center sm:justify-between sm:shadow-md  ">
@@ -31,12 +69,26 @@ function Header({ totalUniqueItems }) {
           className="absolute  bg-no-repeat lg:h-5 lg:w-5 lg:mt-2 lg:ml-3.5 md:h-5 md:w-5 md:mt-2 md:ml-3.5 sm:h-4 sm:w-4 sm:mt-2 sm:ml-3.5"
           size={3}
         />
+        <div className='flex flex-col md:pl-7  text-sm px-2 py-[3px] sm:h-8  sm:my-auto w-[400px] sm:rounded-2xl sm:pl-7 sm:outline-none '>
         <input
           type="text"
           placeholder="Search Re-Serve"
-          className="w-[130px] md:pl-7  text-sm px-2 rounded-full py-[3px] text-gray-400 sm:h-8 sm:w-1/3 sm:w-3/5  bg-slate-100 text-gray-400 sm:my-auto sm:rounded-2xl sm:pl-7 sm:outline-none xl:mr-10"
+          className=" md:pl-7  text-sm px-2 rounded-full py-[3px]  bg-slate-100 text-gray-400 sm:my-auto   outline-none "
+          onChange={(e) =>{
+            setRestaurant(e.target.value);
+            handlesearching(e);            
+          }}
         />
+          <div id='hotellist' className='  text-sm  rounded-b-xl hidden  py-[3px] bg-slate-100 '>
+              {restaurantdata.filter((hotel)=>(hotel.name.toLowerCase().includes(restaurant))).map((item)=>(
+                      <li className='list-none text-gray-400 bg-slate-100 px-2 hover:bg-slate-300 hover:rounded-lg'key={item.id} >{item.name}</li>
+                      
+                  ))}
+          </div>
 
+
+        </div>
+        
         <input
           type="text"
           placeholder="Location"
